5c8e84c57ae11bc9eff8a9fea7f0d70fc563a538
Jang Belche <jangbel99@gmail.com>
2020-06-03 19:35:39 +0100

    Updates

---
+++ precache-manifest.dac4d1a3c356d07ae3ae656900730f88.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1f6967c13f5f82d946f3656fb38c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f4b4bc90c81d5a53339",
+    "url": "/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "84a7804df9cef68d9251",
+    "url": "/static/css/main.879cd0bd.chunk.css"
+  },
+  {
+    "revision": "3f4b4bc90c81d5a53339",
+    "url": "/static/js/2.a92df819.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.a92df819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a7804df9cef68d9251",
+    "url": "/static/js/main.cc1f309d.chunk.js"
+  },
+  {
+    "revision": "400c1075147db57919c4",
+    "url": "/static/js/runtime-main.6f85cc37.js"
+  },
+  {
+    "revision": "3161df8693e04aa0d8ddd4f23c5bd4e8",
+    "url": "/static/media/basketball.3161df86.jpg"
+  },
+  {
+    "revision": "c146fdbd6ca3ce2f730c0d43752ff440",
+    "url": "/static/media/bhai.c146fdbd.jpg"
+  },
+  {
+    "revision": "73b5c8e08845cdbc6e083c5e36bc1c36",
+    "url": "/static/media/bike.73b5c8e0.jpeg"
+  },
+  {
+    "revision": "131ee1e58c7ae0e6b174166c200a79f4",
+    "url": "/static/media/book.131ee1e5.jfif"
+  },
+  {
+    "revision": "f85e8eedd82da21494b9a65dd8851700",
+    "url": "/static/media/brain.f85e8eed.png"
+  },
+  {
+    "revision": "9731d1b785e2547da6a4f3f42564a185",
+    "url": "/static/media/climbing.9731d1b7.jpg"
+  },
+  {
+    "revision": "8aae74f9d91ce0c99f306e27f619ea56",
+    "url": "/static/media/food.8aae74f9.jpg"
+  },
+  {
+    "revision": "b76ea43a1a902551c01865e51d72a7cb",
+    "url": "/static/media/game.b76ea43a.png"
+  },
+  {
+    "revision": "8a533459fc3cb1ae4bcf4241c898b53d",
+    "url": "/static/media/hiking.8a533459.jpg"
+  },
+  {
+    "revision": "2c1010195ab581923d53388f858ec96f",
+    "url": "/static/media/jang.2c101019.jpeg"
+  },
+  {
+    "revision": "2a0d282e02634ce1cec0e574d199038c",
+    "url": "/static/media/man.2a0d282e.jpg"
+  },
+  {
+    "revision": "db738e0423513dd886ce80c225047df9",
+    "url": "/static/media/mario.db738e04.jpg"
+  },
+  {
+    "revision": "abc48fa1b355449d3a2ee0a8d9df39cb",
+    "url": "/static/media/music.abc48fa1.jpg"
+  },
+  {
+    "revision": "c986624ea6b415d6a49b38b3a2d26a0d",
+    "url": "/static/media/pc.c986624e.jpg"
+  },
+  {
+    "revision": "295df950ffb3325d5f60ed1c254483e1",
+    "url": "/static/media/rugby.295df950.jpg"
+  },
+  {
+    "revision": "06ee14bf944794e50aa6c87f696523bd",
+    "url": "/static/media/running.06ee14bf.jpg"
+  },
+  {
+    "revision": "4fb3756d4a31fe3beaf4cc2abad93604",
+    "url": "/static/media/skiing.4fb3756d.jpg"
+  },
+  {
+    "revision": "031d31041dbd9671ddc23a27a86108ee",
+    "url": "/static/media/skybox.031d3104.png"
+  },
+  {
+    "revision": "30db6ac8e21701c3826c8db9e8488f28",
+    "url": "/static/media/swim.30db6ac8.jpg"
+  },
+  {
+    "revision": "4ee00c85db3aec0ca9874804f2b3740c",
+    "url": "/static/media/travel.4ee00c85.jpg"
+  },
+  {
+    "revision": "71a01d413d0b24709441773bc571735c",
+    "url": "/static/media/uwc.71a01d41.gif"
+  }
+]);
